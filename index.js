d8ae6474001f3685fee1d4902de141674cf73e65
Brian Schlenker <bschlenk@umich.edu>
2020-04-12 15:09:54 -0400

    feat: handle rate limit

--- index.js
+++ index.js
@@ -16,12 +16,10 @@ const octokit = new MOctokit.Octokit({
 });
 
 (async () => {
-  const results = await octokit.search.code({
-    q: 'style language:html',
-  });
+  const out = fs.createWriteStream('./results.csv');
 
-  results.data.items.forEach(async (item) => {
-    if (item.path.startsWith('vendor/')) return;
+  for await (const item of codeSearch('style language:html')) {
+    if (shouldSkip(item.path)) continue;
 
     const blob = await octokit.git.getBlob({
       owner: item.repository.owner.login,
@@ -29,16 +27,78 @@ const octokit = new MOctokit.Octokit({
       file_sha: item.sha,
     });
 
-    const content = decode(blob.data.content);
-    const styles = extractStyleTags(content);
+    await checkRateLimit(blob);
 
-    if (styles.length) {
-      console.log('%s:%s', item.repository.full_name, item.path);
-      console.log(styles);
+    const content = decode(blob.data.content);
+    const data = analyzeStyles(content);
+
+    if (data.total) {
+      const file = `${item.repository.full_name}:${item.path}`;
+      const percentSemi = ((data.semi / data.total) * 100).toFixed(2);
+
+      console.log(file);
+      console.log('  %s%% semi (%d total styles)', percentSemi, data.total);
       console.log();
+
+      out.write(`${file},${data.semi},${data.nosemi},${data.total}\n`);
     }
-  });
+  }
+
+  out.close();
 })();
+
+async function* codeSearch(query) {
+  let page = 1;
+
+  while (true) {
+    console.log('* fetching page %d of results', page);
+
+    const results = await handleAbuseDetection(() =>
+      octokit.search.code({
+        q: query,
+        page,
+      }),
+    );
+
+    console.log(results.headers.link);
+
+    if (results.data.items.length === 0) {
+      break;
+    }
+
+    for (const item of results.data.items) {
+      yield item;
+    }
+
+    ++page;
+
+    await checkRateLimit(results);
+  }
+}
+
+function analyzeStyles(fileContents) {
+  let semi = 0;
+  let nosemi = 0;
+
+  const styles = extractStyleTags(fileContents);
+  for (const s of styles) {
+    if (s.trim().endsWith(';')) {
+      ++semi;
+    } else {
+      ++nosemi;
+    }
+  }
+
+  return {
+    semi,
+    nosemi,
+    total: styles.length,
+  };
+}
+
+function wait(time) {
+  return new Promise((res) => setTimeout(res, time));
+}
 
 function extractStyleTags(data) {
   const matches = [...data.matchAll(/style\s*=\s*"([^"]+)"/g)];
@@ -47,4 +107,47 @@ function extractStyleTags(data) {
 
 function decode(contents) {
   return Buffer.from(contents, 'base64').toString('ascii');
+}
+
+function shouldSkip(fname) {
+  return fname.startsWith('vendor/') || fname.endsWith('.py.html');
+}
+
+async function checkRateLimit(response) {
+  const {
+    'x-ratelimit-limit': limit,
+    'x-ratelimit-remaining': remaining,
+    'x-ratelimit-reset': reset,
+  } = response.headers;
+
+  console.log(
+    '* rate limit: %s, remaining: %s, reset: %s',
+    limit,
+    remaining,
+    reset,
+  );
+
+  if (remaining == 0) {
+    const toWait = Math.max(0, parseInt(reset) - Date.now());
+    console.log('* rate limit exceeded - waiting %d ms', toWait);
+    await wait(toWait);
+  }
+}
+
+async function handleAbuseDetection(fn) {
+  let retries = 0;
+  while (retries < 3) {
+    try {
+      return await fn();
+    } catch (err) {
+      const retryAfter = err.headers['retry-after'];
+      console.log(
+        '* triggered abuse detection! waiting for %s seconds (%s)',
+        retryAfter,
+        err,
+      );
+      await wait(1000 * retryAfter);
+      ++retries;
+    }
+  }
 }
